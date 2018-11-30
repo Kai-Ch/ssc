b3841383dc71932a360b0447c46d2e233affabf9
kaichenr <kaichenr@isoftstone.com>
2018-11-30 17:58:50 +0800

    consummate

---
+++ src/main/java/com/ssc/code/entity/ReturnResult.java
@@ -0,0 +1,34 @@
+package com.ssc.code.entity;
+
+public class ReturnResult<T> {
+
+    private String msg;
+
+    private String tag;
+
+    private T data;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
