cc7eab7ee72fd6953f0c6846b0a5698d31692259
kaichenr <kaichenr@isoftstone.com>
2018-12-11 21:18:40 +0800

    修改web.xml（访问不到控制层

--- src/main/java/com/ssc/code/web/TestController.java
+++ src/main/java/com/ssc/code/web/TestController.java
@@ -3,15 +3,23 @@ package com.ssc.code.web;
 import com.ssc.code.entity.ReturnResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/t")
 public class TestController {
 
     @RequestMapping("/test")
+    @ResponseBody
     public ReturnResult test(){
         ReturnResult returnResult = new ReturnResult();
         returnResult.setMsg("33333");
         return returnResult;
+    }
+
+    @RequestMapping("/welcome")
+    public String welcome(){
+        System.out.println("2222");
+        return "welcome";
     }
 }
