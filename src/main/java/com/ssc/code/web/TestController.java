5a5e3a3c5e8abed42f06903c6643ee837eced38d
kaichenr <kaichenr@isoftstone.com>
2018-12-13 09:58:44 +0800

    增加日志

--- src/main/java/com/ssc/code/web/TestController.java
+++ src/main/java/com/ssc/code/web/TestController.java
@@ -3,6 +3,8 @@ package com.ssc.code.web;
 import com.ssc.code.entity.ReturnResult;
 import com.ssc.code.entity.Test;
 import com.ssc.code.service.ITestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +17,12 @@ public class TestController {
     @Autowired
     private ITestService iTestService;
 
+    private Logger log = LoggerFactory.getLogger(TestController.class);
+
     @RequestMapping("/test")
     @ResponseBody
     public ReturnResult test(){
+        log.info("test------");
         Test test = iTestService.test();
         ReturnResult returnResult = new ReturnResult();
         returnResult.setMsg("33333");
